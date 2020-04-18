609f121614dcf70beb1d33e612dcbb9cc76163b3
chanduarepalligit <63819355+chanduarepalligit@users.noreply.github.com>
2020-04-19 02:11:10 +0530

    New Armstrong number method has been added

--- Armstrong Number.py
+++ Armstrong Number.py
@@ -1,4 +1,4 @@
-num = int(input("Enter Number : "))
+num = 153
 k = len(str(num))
 a = list(map(int, str(num)))
 b = list(map(lambda x: x **k, a))
@@ -7,4 +7,15 @@ if sum(b) == num:
 else:
     print("Not an Armstrong number")
 
-print("Branch1 created in pycharm")
+num = 153
+k = len(str(num))
+res = 0
+i = num
+while i > 0:
+    temp = i % 10
+    res = res + temp ** k
+    i = i // 10
+if res == num:
+    print("Armstrong Number")
+else:
+    print("Not an Armstrong Number")
